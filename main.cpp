c7a98c84f3cec9b8053db807e72c2926f1c918b5
annisanadiva <2310631170068@student.unsika.ac.id>
2023-10-15 23:23:34 +0700

    menambahkan file main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    cout << "-------------------------------------------------------------------" << endl;
+    cout << "---------------------------------------------------------------------------" << endl;
     cout << "Nama : Annisa Nur Aulia Nadiva" << endl;
     cout << endl;
     cout << "NPM : 2310631170068" << endl;
@@ -14,7 +14,7 @@ int main()
     cout << "Cita-Cita : Data Scientist" << endl;
     cout << endl;
     cout << "Motivasi Masuk Informatika : Saran dari Mama dan terinspirasi dari Start-Up" << endl;
-    cout << "---------------------------------------------------------------------" << endl;
+    cout << "---------------------------------------------------------------------------" << endl;
     cout << endl;
     return 0;
 }
